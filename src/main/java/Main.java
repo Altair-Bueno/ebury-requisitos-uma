2b723adce7c0bd8b8bd617f9a3b69a5acdef58c6
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-12-04 21:12:55 +0100

    dep: Added new dependencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import com.formdev.flatlaf.FlatIntelliJLaf;
 public class Main {
     public static void main(String[] args) {
+        FlatIntelliJLaf.setup();
         System.out.println("Hello world");
     }
 }
