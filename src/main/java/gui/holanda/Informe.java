dcf198e3f8a6cd315eb93891e383a42af560a77f
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-12-05 13:48:08 +0100

    new: Informe holanda

--- src/main/java/gui/holanda/Informe.java
+++ src/main/java/gui/holanda/Informe.java
@@ -1,4 +1,16 @@
 package gui.holanda;
 
+import javax.swing.*;
+
 public class Informe {
+    private JPanel root;
+    private JTextArea jsonPreviewArea;
+    private JButton generarButton;
+    private JButton exportarButton;
+    private JTabbedPane filterTabbedPane;
+    private JPanel filtroProductoPanel;
+    private JPanel filtroClientePanel;
+    private JPanel healthCheckPanel;
+    private JPanel buttonPanel;
+    private JScrollPane scrollPane;
 }
