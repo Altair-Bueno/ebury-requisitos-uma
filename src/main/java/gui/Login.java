6edaf299f9aa72a858cab31a6822f66680c6b76a
jxtaaa <jlbp@uma.es>
2021-12-05 13:37:06 +0100

    login hecho

--- src/main/java/gui/Login.java
+++ src/main/java/gui/Login.java
@@ -1,4 +1,17 @@
 package gui;
 
+import javax.swing.*;
+
 public class Login {
+    private JPanel panel1;
+    private JPasswordField passwordField;
+    private JTextField textField;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel Image;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        Image = new JLabel(new ImageIcon("resources/EburyLogo.png"));
+    }
 }
