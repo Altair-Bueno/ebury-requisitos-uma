7764ac9b5432219c4ba521eff221e3384fba7e03
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-12-05 13:31:31 +0100

    new: informe alemania

--- src/main/java/gui/alemania/Informe.java
+++ src/main/java/gui/alemania/Informe.java
@@ -1,4 +1,12 @@
 package gui.alemania;
 
+import javax.swing.*;
+
 public class Informe {
+    private JButton primerInforme;
+    private JTable csvPreview;
+    private JButton informeSemanal;
+    private JButton enviarSFTP;
+    private JButton exportar;
+    private JScrollPane scrollPane;
 }
